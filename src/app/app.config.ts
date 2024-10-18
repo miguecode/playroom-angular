322aba014c8635713933c57fdd4113615f4a5dc8
Leumig <junmigue7@gmail.com>
2024-10-18 19:50:30 -0300

    cambios en el entorno

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,15 +5,16 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideHttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), importProvidersFrom(provideFirebaseApp(() => initializeApp({
-    "projectId":"sala-de-juegos-82d5a",
-    "appId":"1:28981568672:web:5e36f916525d69954e15c5",
-    "storageBucket":"sala-de-juegos-82d5a.appspot.com",
-    "apiKey":"AIzaSyDvGEdI5mnuRerkMPrKYsuVNdJzSh0a7lA",
-    "authDomain":"sala-de-juegos-82d5a.firebaseapp.com",
-    "messagingSenderId":"28981568672",
-    "measurementId":"G-3MY1DHJ0HG"}))), 
-    importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore()))]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(environment.firebaseConfig))
+    ),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideFirestore(() => getFirestore()))
+  ]
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,24 +1,12 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
   firebaseConfig: {
+    projectId: "sala-de-juegos-82d5a",
+    appId: "1:28981568672:web:5e36f916525d69954e15c5",
+    storageBucket: "sala-de-juegos-82d5a.appspot.com",
     apiKey: "AIzaSyDvGEdI5mnuRerkMPrKYsuVNdJzSh0a7lA",
     authDomain: "sala-de-juegos-82d5a.firebaseapp.com",
-    projectId: "sala-de-juegos-82d5a",
-    storageBucket: "sala-de-juegos-82d5a.appspot.com",
     messagingSenderId: "28981568672",
-    appId: "1:28981568672:web:5e36f916525d69954e15c5"
+    measurementId: "G-3MY1DHJ0HG"
   }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
